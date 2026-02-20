9c2d5e0ecec3e67cb0b212f734baac85eb0f2038
dudupc098-maker <dudupc098@gmail.com>
2026-02-20 00:01:28 -0300

    Create utm-saver.tsx

---
+++ src/components/utm-saver.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import { useEffect } from "react";
+import { saveUTMs } from "@/lib/utm";
+
+export default function UTMSaver() {
+  useEffect(() => {
+    saveUTMs();
+  }, []);
+
+  return null;
+}
